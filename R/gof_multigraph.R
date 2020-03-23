0e7bfa2d714227c605ed3305c4a6ea4ff158a7a1
termeh <termeh.shafie@manchester.ac.uk>
2020-03-23 10:19:29 +0000

    Functions added

---
+++ R/gof_multigraph.R
@@ -0,0 +1,109 @@
+#' @title Goodness of Fit Tests for Random Multigraph Models
+#' @description gives the degree sequence of observed graph or multigraph
+#' @param adj integer matrix 
+#' @param type equals 'graph' if adjacency matrix is for graphs (default), 
+#' equals 'multigraph' if it is the equivalence of adjacency matrix for multigraphs 
+#' (with diagonal double counted)
+#' model = either RSM, IEAS or ISA with modelled degree sequence deg.mod
+#' vechyp= hypothetical degree sequence,  
+#' hypothetical degree sequence:
+#'   if 'IEA': d = deg.hyp fully specified
+#'   if 'ISA': p = deg.hyp/2m fully specified
+#'   if 'IEA' and deg.hyp = 0, d estimated from edge mutliplicity sequence
+#'   if 'ISA' and deg.hyp = 0, p estimated from edge mutliplicity sequence
+#' @return vector of integers representing the degree sequence of the multigraph
+#' @details  to be completed
+#' @author Termeh Shafie
+#' @references Shafie, T. (2015). A Multigraph Approach to Social Network Analysis. *Journal of Social Structure*, 16.
+#' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. *The Journal of Mathematical Sociology*, 40(4), 239-264.
+#' @examples
+#' 
+gof_multigraph <- function(m, model, deg.mod, hyp, deg.hyp) {
+  n <- length(deg.mod)
+  r <- choose(n+1, 2)
+  
+  # model specification: IEAS or ISA
+  if(model == 'IEAS'){
+    Q.seq <- edge_assignment_probs(m, deg.mod, model = 'IEAS')
+    # use IEA model to find probabilities of each multiplicity sequence/multigraph
+    m.seq <- nsumk(r, m)
+    prob.mg <-  apply(m.seq, 1, function(x) dmultinom(x, prob = Q.seq))
+  } 
+  else if(model == 'ISA'){
+    Q.seq <- edge_assignment_probs(m, deg.mod, model = 'ISA')
+    # use ISA model to find probabilities of each multiplicity sequence/multigraph
+    m.seq <- nsumk(r, m)
+    prob.mg <-  apply(m.seq, 1, function(x) dmultinom(x, prob = Q.seq))
+  }
+  else if(model == 'RSM'){
+    # use RSM model to find probabilities of each multiplicity sequence/multigraph
+    rsm <- rsm_model(deg.mod)
+    m.seq <- rsm$m.seq
+    prob.mg <- rsm$prob.dists[ ,1]
+  }
+  
+  # according to hypothesis, get the values of test statistics 
+  # hypothesis IEAS
+  if(hyp == 'IEAS'){
+    if(sum(deg.hyp) > 0){
+      dof <- r - 1
+      # fully specified with deg.hyp
+      Q.seq <- edge_assignment_probs(m, deg.hyp, model = 'IEAS')
+      moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
+    }
+    else if(sum(deg.hyp) == 0){
+      dof <- r - n
+      # estimated dest from data
+      deg.est <- matrix(0, nrow(m.seq), n)
+      for(i in 1:nrow(m.seq)){
+        M <- matrix(0, n, n)
+        M[lower.tri(M, diag = TRUE)] <- 1
+        M[M == 1] <- m.seq[i, ]
+        M <- M + t(M)
+        deg.est[i, ] <- colSums(M)
+      }
+      Q.seq <- matrix(0, nrow(m.seq), r)
+      for(d in 1:nrow(deg.est)){
+        deg.seq <- deg.est[d, ]
+        Q.seq[d, ] <- edge_assignment_probs(m, deg.seq, model = 'IEAS')
+      }
+      moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq) 
+    }
+  }
+  # hypothesis ISA 
+  else if(hyp == 'ISA'){
+    if(sum(deg.hyp) > 0){
+      dof <- r - 1
+      # fully specified with deg.hyp
+      Q.seq <- edge_assignment_probs(m, deg.hyp, model = 'ISA')
+      moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
+    }
+    else if(sum(deg.hyp) == 0){
+      dof <- r - n
+      # estimated dest from data
+      deg.est <- matrix(0, nrow(m.seq), n)
+      for(i in 1:nrow(m.seq)){
+        M <- matrix(0, n, n)
+        M[lower.tri(M, diag = TRUE)] <- 1
+        M[M == 1] <- m.seq[i, ]
+        M <- M + t(M)
+        deg.est[i, ] <- colSums(M)/(2*m)
+      }
+      Q.seq <- matrix(0, nrow(m.seq), r)
+      for(d in 1:nrow(deg.est)){
+        deg.seq <- deg.est[d, ]
+        Q.seq[d, ] <- edge_assignment_probs(m, deg.seq, model = 'ISA')
+      }
+      moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq) 
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ R/gof_stats.R
@@ -0,0 +1,187 @@
+#' @title Moments of Goodness of Fit Statistics
+#' @description
+#' @param
+#' @return
+#' @details  To be completed
+#' @author Termeh Shafie
+#' @references Shafie, T. (2015). A Multigraph Approach to Social Network Analysis. *Journal of Social Structure*, 16.
+#' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. *The Journal of Mathematical Sociology*, 40(4), 239-264.
+#' @examples
+#'
+gof_stats <- function(m, dof, m.seq, prob.mg, Q.seq) {
+  # the chi-square distribution with dof degrees of freedom, cv = critical value
+  cv <- dof + sqrt(8 * dof)
+  prob.Chi <- pchisq(cv, dof) # prob(chi(dof) < cv)
+  alpha <- 1 - prob.Chi # prob(chi(dof) > cv )
+  # S = Pearson goodness-of-fit statistic
+  # Observed values = O, Expected values = E
+  O = m.seq
+  E = m * Q.seq
+  if (is.vector(E)) {
+    S = (t(O) - E) ^ 2 / E
+    S[is.infinite(S) | is.na(S)] = 0
+    S = colSums(S)
+  } else{
+    S = (O - E) ^ 2 / E
+    S[is.infinite(S) | is.na(S)] = 0
+    S = rowSums(S)
+  }
+  S <-
+    round(S, 5) # if you wish to round before finding unique values to make outcome space smaller
+  S.uni <- sort(unique(S))
+  # probability distribution of the S values
+  prob.tmp <- vector()
+  prob.S <- vector()
+  for (i in 1:length(S.uni)) {
+    for (j in 1:length(S)) {
+      if (S.uni[i] == S[j]) {
+        prob.tmp <- c(prob.tmp, prob.mg[j])
+      }
+    }
+    prob.S[i] <- sum(prob.tmp)
+    prob.tmp <- vector()
+  }
+  cumprob.S <- cumsum(prob.S)
+  ExpS <- sum(S.uni * prob.S)
+  VarS <- sum((S.uni ^ 2 * prob.S)) - ExpS ^ 2
+  # prob(Shat > cv)
+  S.g.cv <- sum(prob.S[S.uni > cv])
+  # prob(Shat > cv)
+  cvS <- ExpS + 2 * sqrt(VarS)
+  S.g.cvS <- sum(prob.S[S.uni > cvS])
+  S.out <-
+    round(cbind(ExpS, VarS, cv, alpha, S.g.cv, cvS, S.g.cvS), 5) # for output
+  # approximate statistics (adjusted chi square): S' and S''
+  S.prim <- S.uni * (floor(ExpS) / ExpS)
+  ExpS.prim <- ExpS
+  VarS.prim <- (2 * ExpS ^ 2) / (floor(ExpS))
+  S.bis <- S.uni * (dof / ExpS)
+  ExpS.bis <- ExpS
+  VarS.bis <- (2 * ExpS ^ 2) / dof
+  
+  # D = divergence goodness-of-fit statistic
+  if (is.vector(E)) {
+    D <- (O / m) * log2(O %*% diag(1 / E))
+    D[is.infinite(D) | is.na(D)] = 0
+    D <- rowSums(D)
+  } else{
+    D <- (O / m) * log2(O * (1 / E))
+    D[is.infinite(D) | is.na(D)] = 0
+    D <- rowSums(D)
+  }
+  # probability distribution of the D values
+  D <- round(D, 5) # if you wish to round
+  #] D.uni <- sort(unique(D))
+  # prob.tmp <- vector()
+  # prob.D <- vector()
+  # for(i in 1:length(D.uni)){
+  #   for(j in 1:length(D)){
+  #     if(D.uni[i] == D[j]){
+  #       prob.tmp <- c(prob.tmp, prob.mg[j])
+  #     }
+  #   }
+  #   prob.D[i] <- sum(prob.tmp)
+  #   prob.tmp <- vector()
+  # }
+  # cumprob.D <- cumsum(prob.D)
+  # ExpD <- sum(D.uni*prob.D)
+  # VarD <- sum((D.uni^2*prob.D))-ExpD^2
+  # # prob(Shat > cv)
+  # D.g.cv <- sum(prob.D[D.uni > cv])
+  # # prob(Shat > cv)
+  # cvD <- ExpD+2*sqrt(VarD)
+  
+  # probability distribution of the A values
+  # A <- round(A,3) # if you wish to round
+  A <- (2 * m * D) / log2(exp(1)) # the asymptotic T statistics
+  A.uni <- sort(unique(A))
+  prob.tmp <- vector()
+  prob.A <- vector()
+  for (i in 1:length(A.uni)) {
+    for (j in 1:length(A)) {
+      if (A.uni[i] == A[j]) {
+        prob.tmp <- c(prob.tmp, prob.mg[j])
+      }
+    }
+    prob.A[i] <- sum(prob.tmp)
+    prob.tmp <- vector()
+  }
+  cumprob.A <- cumsum(prob.A)
+  ExpA <- sum(A.uni * prob.A)
+  VarA <- sum((A.uni ^ 2 * prob.A)) - ExpA ^ 2
+  # prob(Shat > cv)
+  A.g.cv <- sum(prob.A[A.uni > cv])
+  # prob(Shat > cv)
+  cvA <- ExpA + 2 * sqrt(VarA)
+  A.g.cvA <- sum(prob.A[A.uni > cvA])
+  A.out <-
+    round(cbind(ExpA, VarA, cv, alpha, A.g.cv, cvA, A.g.cvA), 5)
+  # approximate statistics (adjusted chi square): S' and S''
+  A.prim <- A.uni * (floor(ExpA) / ExpA)
+  ExpA.prim <- ExpA
+  VarA.prim <- (2 * ExpA ^ 2) / (floor(ExpA))
+  A.bis <- A.uni * (dof / ExpA)
+  ExpA.bis <- ExpA
+  VarA.bis <- (2 * ExpA ^ 2) / dof
+  
+  # output 1: probability distributions of S and A
+  prob.Sout <-
+    as.data.frame(round(cbind(S.uni, prob.S, cumprob.S), 5))
+  colnames(prob.Sout) <- c('S=s', 'P(S=s)', 'P(S<s)')
+  prob.Aout <-
+    as.data.frame(round(cbind(A.uni, prob.A, cumprob.A), 5))
+  colnames(prob.Aout) <- c('A=a', 'P(A=a)', 'P(A<a)')
+  
+  # output 2: summary for statistics S and A
+  gof.sum <- as.data.frame(rbind(S.out, A.out))
+  stat <- c('S', 'A')
+  gof.sum <- cbind(stat, gof.sum)
+  colnames(gof.sum) <-
+    c('stat',
+      'exp',
+      'var',
+      'cv',
+      'alpha',
+      'stat>cv',
+      'cv(stat)',
+      'stat>cv(stat)')
+  
+  
+  # output 3: moments of approximate statistics (just differene in variance)
+  Exp.out <-
+    cbind(ExpS, ExpS.prim, ExpS.bis, ExpA, ExpA.prim, ExpA.bis)
+  Var.out <-
+    cbind(VarS, VarS.prim, VarS.bis, VarA, VarA.prim, VarA.bis)
+  moms.apx <- as.data.frame(rbind(Exp.out, Var.out))
+  mom.lab <- c('Exp', 'Var')
+  moms.apx <- cbind(mom.lab, moms.apx)
+  colnames(moms.apx) <-
+    c('moment', 'S', 'Sprim', 'Sbis', 'A', 'Aprim', 'Abis')
+  
+  # output 4: power approixmations with S and A
+  tmp <- cv * floor(ExpS) / ExpS
+  powerS.prim <- pchisq(tmp, floor(ExpS))
+  tmp2 <- cv * (2 * ExpS ^ 2 / dof) / ExpS
+  powerS.bis <- pchisq(tmp2, floor(ExpS))
+  tmp <- cv * floor(ExpA) / ExpA
+  powerA.prim <- pchisq(tmp, floor(ExpA))
+  tmp2 <- cv * (2 * ExpA ^ 2 / dof) / ExpA
+  powerA.bis <- pchisq(tmp2, floor(ExpA))
+  
+  power.apx <- as.data.frame(round(cbind(
+    powerS.prim, powerS.bis, powerA.prim, powerA.bis
+  ), 5))
+  colnames(power.apx) <- c('Sprim', 'Sbis', 'Aprim', 'Abis')
+  
+  # output 5: plot the distributions of S and A, together with Chi
+  listout <-
+    list(
+      "probS" = prob.Sout,
+      "probA" = prob.Aout,
+      "summmary" = gof.sum,
+      "moments.apx" = moms.apx,
+      "power.apx" = power.apx,
+      "degrees.of.freedom" = dof
+    )
+  return(listout)
+}
