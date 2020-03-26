87c11ba9d80a3fbfc00288354f88eb1a56a51085
termeh <termeh.shafie@manchester.ac.uk>
2020-03-26 08:32:39 +0000

    Updates

--- R/gof_multigraph.R
+++ R/gof_multigraph.R
@@ -20,90 +20,87 @@
 #'
 gof_multigraph <- function(m, model, deg.mod, hyp, deg.hyp) {
   n <- length(deg.mod)
-  r <- choose(n+1, 2)
+  r <- choose(n + 1, 2)
 
   # model specification: IEAS or ISA
-  if(model == 'IEAS'){
+  if (model == 'IEAS') {
     Q.seq <- edge_assignment_probs(m, deg.mod, model = 'IEAS')
     # use IEA model to find probabilities of each multiplicity sequence/multigraph
     m.seq <- nsumk(r, m)
-    prob.mg <-  apply(m.seq, 1, function(x) dmultinom(x, prob = Q.seq))
+    prob.mg <-
+      apply(m.seq, 1, function(x)
+        dmultinom(x, prob = Q.seq))
   }
-  else if(model == 'ISA'){
+  else if (model == 'ISA') {
     Q.seq <- edge_assignment_probs(m, deg.mod, model = 'ISA')
     # use ISA model to find probabilities of each multiplicity sequence/multigraph
     m.seq <- nsumk(r, m)
-    prob.mg <-  apply(m.seq, 1, function(x) dmultinom(x, prob = Q.seq))
+    prob.mg <-
+      apply(m.seq, 1, function(x)
+        dmultinom(x, prob = Q.seq))
   }
-  else if(model == 'RSM'){
+  else if (model == 'RSM') {
     # use RSM model to find probabilities of each multiplicity sequence/multigraph
     rsm <- rsm_model(deg.mod)
     m.seq <- rsm$m.seq
-    prob.mg <- rsm$prob.dists[ ,1]
+    prob.mg <- rsm$prob.dists[, 1]
   }
 
   # according to hypothesis, get the values of test statistics
   # hypothesis IEAS
-  if(hyp == 'IEAS'){
-    if(sum(deg.hyp) > 0){
+  if (hyp == 'IEAS') {
+    if (sum(deg.hyp) > 0) {
       dof <- r - 1
       # fully specified with deg.hyp
       Q.seq <- edge_assignment_probs(m, deg.hyp, model = 'IEAS')
       moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
     }
-    else if(sum(deg.hyp) == 0){
+    else if (sum(deg.hyp) == 0) {
       dof <- r - n
       # estimated dest from data
       deg.est <- matrix(0, nrow(m.seq), n)
-      for(i in 1:nrow(m.seq)){
+      for (i in 1:nrow(m.seq)) {
         M <- matrix(0, n, n)
         M[lower.tri(M, diag = TRUE)] <- 1
-        M[M == 1] <- m.seq[i, ]
+        M[M == 1] <- m.seq[i,]
         M <- M + t(M)
-        deg.est[i, ] <- colSums(M)
+        deg.est[i,] <- colSums(M)
       }
       Q.seq <- matrix(0, nrow(m.seq), r)
-      for(d in 1:nrow(deg.est)){
-        deg.seq <- deg.est[d, ]
-        Q.seq[d, ] <- edge_assignment_probs(m, deg.seq, model = 'IEAS')
+      for (d in 1:nrow(deg.est)) {
+        deg.seq <- deg.est[d,]
+        Q.seq[d,] <-
+          edge_assignment_probs(m, deg.seq, model = 'IEAS')
       }
       moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
     }
   }
   # hypothesis ISA
-  else if(hyp == 'ISA'){
-    if(sum(deg.hyp) > 0){
+  else if (hyp == 'ISA') {
+    if (sum(deg.hyp) > 0) {
       dof <- r - 1
       # fully specified with deg.hyp
       Q.seq <- edge_assignment_probs(m, deg.hyp, model = 'ISA')
       moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
     }
-    else if(sum(deg.hyp) == 0){
+    else if (sum(deg.hyp) == 0) {
       dof <- r - n
       # estimated dest from data
       deg.est <- matrix(0, nrow(m.seq), n)
-      for(i in 1:nrow(m.seq)){
+      for (i in 1:nrow(m.seq)) {
         M <- matrix(0, n, n)
         M[lower.tri(M, diag = TRUE)] <- 1
-        M[M == 1] <- m.seq[i, ]
+        M[M == 1] <- m.seq[i,]
         M <- M + t(M)
-        deg.est[i, ] <- colSums(M)/(2*m)
+        deg.est[i,] <- colSums(M) / (2 * m)
       }
       Q.seq <- matrix(0, nrow(m.seq), r)
-      for(d in 1:nrow(deg.est)){
-        deg.seq <- deg.est[d, ]
-        Q.seq[d, ] <- edge_assignment_probs(m, deg.seq, model = 'ISA')
+      for (d in 1:nrow(deg.est)) {
+        deg.seq <- deg.est[d,]
+        Q.seq[d,] <-
+          edge_assignment_probs(m, deg.seq, model = 'ISA')
       }
       moms <- gof_stats(m, dof, m.seq, prob.mg, Q.seq)
     }
   }
 }
-
-
-
-
-
-
-
-
-
