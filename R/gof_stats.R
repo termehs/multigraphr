8e026c85458f5daa45cb4725042525933b5e56ad
termeh <termeh.shafie@manchester.ac.uk>
2020-06-15 09:08:48 +0100

    updated description

--- R/gof_stats.R
+++ R/gof_stats.R
@@ -1,10 +1,29 @@
-#' @title Moments of Goodness of Fit Statistics
-#' @description Calculates the probability distribution and moments of Pearson and divergence statistics
-#'of goodness of fit when testing various multigraph hypotheses against different random multigraoh models.
-#' @param
+#' @title Goodness of Fit Test Statistics
+#' @description  Goodness of fits tests of multigraph models using Pearson (S) and information divergence (A) tests statistics
+#' under the random stub matching (RSM) and by independent edge assignments (IEA) model,
+#' where the latter is either independent edge assignments of stubs (IEAS) or independent stub assignment (ISA)
+#' @param m number of edges
+#' @param model = assumed model, either RSM, IEAS or ISA
+#' @param deg.mod = modelled degree sequence, vector of integers where even sum
+#' @param hyp = hypothesis, either IEAS or ISA
+#' @param deg.hyp = hypothetical degree sequence, vector of integers where even sum
+#'  hypothetical degree sequence:
+#'   if 'IEAS': simple IEAS hypothesis with fully specified degree sequence deg.hyp
+#'   if 'ISA': simple ISA hypothesis with with fully specified stub assignment probabilities deg.hyp/2m
+#'   if 'IEAS': and deg.hyp = 0: composite IEAS hypothesis with edge mutliplicity sequence estimated from data
+#'   if 'ISA' and deg.hyp = 0: composite ISA hypothesis with edge mutliplicity sequence estimated from data
 #' @return
-#' @details  To be completed
+#'  \item{probS}{Probability distribution of Pearson statistic S}
+#'  \item{probA}{Probability distribution of divergence statistic A}
+#'  \item{summary}{Expected value and variances of test statistics, critical values and significance level
+#'  according to asymptotic $\chi^2$-distribution, power of tests}
+#'  \item{adjusted.stats}{Expected value and variances for adjusted test statistics, preferred adjusted statistics}
+#'  \item{adjusted.chi2}{Degrees of freedom for adjusted $\chi^2$-distribution}
+#'  \item{power.apx}{Power approximations according to adjusted statistics}
+#'  \item{degrees.of.freedom}{Degrees of freedom for test performed}
+#' @details  to be completed
 #' @author Termeh Shafie
+#' @seealso [gof_multigraph]
 #' @references Shafie, T. (2015). A Multigraph Approach to Social Network Analysis. *Journal of Social Structure*, 16.
 #' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. *The Journal of Mathematical Sociology*, 40(4), 239-264.
 #' @examples
