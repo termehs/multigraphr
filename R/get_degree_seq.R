628d3b2e3cb570570ef087fa048fae518014d89f
termeh <termeh.shafie@manchester.ac.uk>
2020-06-23 08:39:04 +0100

    updated description

--- R/get_degree_seq.R
+++ R/get_degree_seq.R
@@ -1,16 +1,22 @@
 #' @title Degree sequence of a multigraph
-#' @description Gives the degree sequence given the adjancecy matrix of an observed graph or multigraph
+#' @description Gives the degree sequence of the adjancecy matrix of an observed graph or multigraph
 #' @param adj Matrix of integers
 #' @param type Equals 'graph' if adjacency matrix is for graphs (default),
 #' equals 'multigraph' if it is the equivalence of the adjacency matrix for multigraphs
 #' (with the elements of the matrix diagonal double counted)
 #' @return Vector of integers representing the degree sequence
-#' @details To be completed
+#' @details Gives the degree sequence of the adjancecy matrix of an observed graph or multigraph
 #' @author Termeh Shafie
 #' @references Shafie, T. (2015). A Multigraph Approach to Social Network Analysis. \emph{Journal of Social Structure}, 16.
 #' \cr
 #' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. \emph{The Journal of Mathematical Sociology}, 40(4), 239-264.
 #' @examples
+#'  ## Adjancency matrix for undirected network with 3 nodes
+#'  A <-  matrix(c(0, 1, 2, 1, 2, 1, 2, 1, 2), nrow=3, ncol=3)
+#'  #If A represents a graph
+#'  deg.seq <- get_degree_seq(adj = A, type = 'graph')
+#'  #If A represents a multigraph
+#'  deg.seq <- get_degree_seq(adj = A, type = 'multigraph')
 #' @export
 #'
 get_degree_seq <- function(adj, type = 'multigraph') {
