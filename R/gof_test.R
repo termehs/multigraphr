57c912f2c89f6a17660834fe902f3d332dfac00b
termeh <termeh.shafie@manchester.ac.uk>
2021-07-08 10:24:47 +0100

    minor typo fixes

--- R/gof_test.R
+++ R/gof_test.R
@@ -2,12 +2,12 @@
 #' @description  Goodness of fits between observed multigraph and specified RSM or IEAS hypotheses
 #' using using Pearson (S) and information divergence (A) tests statistics
 #' @param m integer giving number of edges in multigraph
-#' @param dof  integer giving degrees of freedom of test, 
+#' @param dof  integer giving degrees of freedom of test,
 #' r-1 for simple hypotheses and r-n for composite hypotheses where $r = \binom{n+1}{2}$
 #' (consider changing these according to preferred adjusted chi-square distributions)
 #' @return
 #'  \item{summary}{Table including observed test statistics S and A, degrees of freedom for
-#'  asymptotic chi-square distribution, and p-value for each test statistics. 
+#'  asymptotic chi-square distribution, and p-value for each test statistics.
 #' @details The tests are performed using goodness-of-fit measures between the
 #' edge multiplicity sequence of an observed multigraph,
 #' and the expected multiplicity sequence according to a simple or composite IEAS hypothesis.
@@ -20,16 +20,16 @@
 #' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. The Journal of Mathematical Sociology, 40(4), 239-264.
 #' @examples
 #' ## adjacency matrix of observed network (multigraph), n = 4 nodes , m = 15 edges
-#' m_F_PT <- t(matrix(c( 0,	1,	0,	3,
-#'                       0,	0,	1,	6,
-#'                       0,	0,	0,	3,
-#'                       0,	0,	0,	1), nrow= 4, ncol=4))
+#' m_F_PT <- t(matrix(c( 0, 1, 0, 3,
+#'                       0, 0, 1, 6,
+#'                       0, 0, 0, 3,
+#'                       0, 0, 0, 1), nrow= 4, ncol=4))
 #' # Testing a simple IEAS hypothesis with degree sequence [4 4 8 14]
-#' gof_test(adj, type, 'IEAS', c(4,4,8,14), 9) 
+#' gof_test(adj, type, 'IEAS', c(4,4,8,14), 9)
 #' # Testing a composite IEAS hypothesis
-#' gof_test(adj, type, 'IEAS', 0, 6)                       
+#' gof_test(adj, type, 'IEAS', 0, 6)
 #' @export
-#' 
+#'
 gof_test <- function(adj, type, hyp, deg.hyp, dof) {
 m <- sum(adj) / 2
 n <- nrow(adj)
@@ -54,7 +54,7 @@ if (sum(deg.hyp) == 0) {
   # D = divergence goodness-of-fit statistic
   D <- (O / m) * log2(O / E)
   D[is.infinite(D) | is.na(D)] = 0
-  A <- (2 * m * D) / log2(exp(1)) 
+  A <- (2 * m * D) / log2(exp(1))
   A <- round(sum(A),3)
   } else if (sum(deg.hyp) > 0) {
 Q.seq <- edge_assignment_probs(m, deg.hyp, hyp)
@@ -66,7 +66,7 @@ E = m * Q.seq
 # D = divergence goodness-of-fit statistic
   D <- (O / m) * log2(O / E)
   D[is.infinite(D) | is.na(D)] = 0
-  A <- (2 * m * D) / log2(exp(1)) 
+  A <- (2 * m * D) / log2(exp(1))
   A <- round(sum(A),3)
 }
 # p-values
