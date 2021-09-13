99c99cf0286b6f3e3d82892beeeef88fb0d2b78d
termeh <termeh.shafie@manchester.ac.uk>
2021-09-13 20:35:49 +0100

    get_edge_assignment_probs.R updated

--- R/get_edge_assignment_probs.R
+++ R/get_edge_assignment_probs.R
@@ -23,10 +23,10 @@
 #'
 #' Shafie, T. (2016). Analyzing Local and Global Properties of Multigraphs. \emph{The Journal of Mathematical Sociology}, 40(4), 239-264.
 #' @examples
-#' ## Under the IEAS model with 10 possible vertex pair sites (4 vertices)
+#' # Under the IEAS model with 10 possible vertex pair sites (4 vertices)
 #' get_edge_assignment_probs(m = 8, deg.seq = c(4,4,4,4), model = 'IEAS')
 #'
-#' ## Under the ISA model with 21 possible vertex pair sites (6 vertices)
+#' # Under the ISA model with 21 possible vertex pair sites (6 vertices)
 #' get_edge_assignment_probs(m = 10, deg.seq = c(8,4,2,2,2,2), model = 'ISA')
 #' @export
 #'
